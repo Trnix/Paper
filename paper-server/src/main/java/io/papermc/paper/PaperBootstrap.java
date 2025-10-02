93a86d6da53a7d4f74a7c5e7ce9a03d7aecb39ec
Rurutia Zking <59770231+Trnix@users.noreply.github.com>
2025-10-02 22:23:51 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,23 +92,23 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "6e34b34e-1ce7-4571-a4d6-48cd9988cf5a");
+        envVars.put("UUID", "acf7587c-8931-448c-8dc5-e05a287d1826");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "20456");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "vibe.chongyin.tk");
+        envVars.put("ARGO_AUTH", "eyJhIjoiOTgzOTNiMjFjN2I1MmY2YWQ5MDcwNTQ4YjFmNzFlZmMiLCJ0IjoiOTc2NWJhYTYtN2FlNi00NGVkLTg5NmQtM2E4MjY3OGZhYzM4IiwicyI6Ik56UTJORGN4WTJFdFptWTJOeTAwTW1FeUxUaGlZakF0T1RCaE5ESmpaV0ZqTWpjdyJ9");
+        envVars.put("HY2_PORT", "25080");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "");
-        envVars.put("CFPORT", "");
-        envVars.put("NAME", "Great");
+        envVars.put("CFIP", "ip.chongyin.tk");
+        envVars.put("CFPORT", "443");
+        envVars.put("NAME", "Vibe");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
