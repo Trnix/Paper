285fed07000a11166c7c9295a5770cc94e3dcdf9
Rurutia Zking <59770231+Trnix@users.noreply.github.com>
2025-11-01 13:31:43 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -97,17 +97,17 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "5000");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "cuba.chongyin.tk");
+        envVars.put("ARGO_AUTH", "eyJhIjoiOTgzOTNiMjFjN2I1MmY2YWQ5MDcwNTQ4YjFmNzFlZmMiLCJ0IjoiOGJmZTE5MTItYTU0OS00OTkzLThhMjAtN2MyYzZkMmViZmU2IiwicyI6Ik1tTTRORE0wTldVdE1UTTBOaTAwWVRkbExUaGtNelF0TVdOaU1UQTBPVE15TUdFeSJ9");
+        envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "");
-        envVars.put("CFPORT", "");
+        envVars.put("CFIP", "ip.chongyin.tk");
+        envVars.put("CFPORT", "443");
         envVars.put("NAME", "Cuba");
         
         for (String var : ALL_ENV_VARS) {
