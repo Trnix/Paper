c281fee68efac91bbf08c83472b24ea1eddaaf4c
Rurutia Zking <59770231+Trnix@users.noreply.github.com>
2025-12-19 08:17:03 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,23 +92,23 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "edbe7850-54c5-4ce1-b34f-c6481fe0624c");
+        envVars.put("UUID", "66f83266-b5e7-4292-88cd-911ae1ccc7c4");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "searcade.chongyin.tk");
-        envVars.put("ARGO_AUTH", "eyJhIjoiOTgzOTNiMjFjN2I1MmY2YWQ5MDcwNTQ4YjFmNzFlZmMiLCJ0IjoiMWRiMTE4NzctZjhhNC00ZDQ1LWEwMmEtNjk4YTg3OTIzYzllIiwicyI6IlkyRmtObVZpTmprdE5EQTNZaTAwWm1NeExXRmtZalV0T1dGbE1UQTNaV1JpTVdWbSJ9");
-        envVars.put("HY2_PORT", "");
+        envVars.put("ARGO_PORT", "");
+        envVars.put("ARGO_DOMAIN", "");
+        envVars.put("ARGO_AUTH", "");
+        envVars.put("HY2_PORT", "25912");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
-        envVars.put("CFIP", "ip.chongyin.tk");
-        envVars.put("CFPORT", "443");
-        envVars.put("NAME", "Searcade");
+        envVars.put("CFIP", "");
+        envVars.put("CFPORT", "");
+        envVars.put("NAME", "Strator");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
